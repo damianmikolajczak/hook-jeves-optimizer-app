356431d8380096fd245bcfbb6d0557e0bf0bc16c
damianmikolajczak <damian_mikolajczak@icloud.com>
2021-06-26 13:37:35 +0200

    Algorithm description added

--- Hook - Jeves optimazer/OptimazerModel.swift
+++ Hook - Jeves optimazer/OptimazerModel.swift
@@ -53,21 +53,29 @@ struct HookJavesOptimazer {
         }
     }
 
+    // This function is used to search for the next base point. First of all, a log is added that the program started searching.
     func searchPoint(currentBasePoint: Point) -> Point?{
         statusBox.insertText("Searching for new point\n")
+        // At the begining we reinitialaze the local parameters.
         var basePoint = currentBasePoint
         var newBasePointFounded: Bool = false
         var nextPoint = Point(0,0)
         var currentMinimalValue = f(basePoint)
         var valueInNextPoint = Double()
         
+        /*
+         * Using a for loop the program goes through all directions in the directions base array
+         * Then we get a point in the given direction and calculate the functions value.
+         */
         for k in 0..<(directionsBase.count) {
             nextPoint = basePoint + step * directionsBase[k]
             valueInNextPoint = f(nextPoint)
+            // If the value in the new point is lower then in the previous point then we store this point as the new base point as set "true" to the flag that says if a new base point was found.
             if valueInNextPoint < currentMinimalValue {
                 currentMinimalValue = valueInNextPoint
                 basePoint = nextPoint
                 newBasePointFounded = true
+            // If the value is bigger that in the previous point then we change the direction and repeat the above described steps.
             } else {
                 nextPoint = basePoint - step * directionsBase[k]
                 valueInNextPoint = f(nextPoint)
@@ -78,23 +86,45 @@ struct HookJavesOptimazer {
                 }
             }
         }
+        
+        /*
+         * After going through the whole directions base we return the new base poin (if one was founded).
+         * If no point was founded, then this function returns a nil.
+         */
+        
         return newBasePointFounded ? basePoint : nil
     }
 
+    // This function implements the main loop of the Hook-Jeves algorithm. First of all we print a log with the start value.
     mutating func startOptimazer() -> (Point, Double, [Point]) {
-        //Etap roboczy: statusBox.insertText("")
         statusBox.insertText("Start value: \(f(argument))\n\n")
+        /*
+         * Ad the begining the main aprameters are initialized with default values.
+         * The foundedPoint table is used to print the path in the graph view.
+         * Because the user can run multiple search request during on run, the described table should be cleared.
+         */
         var iteration: Int = 0
         var direction = Point(0,0)
         foundedPoints.removeAll()
-        foundedPoints.append(argument)         //Dodanie punktu startowego
+        foundedPoints.append(argument)
+        // The algorithm runs as long as the methods step is bigger then the required accuracy.
         while ((step > sigma)){
+            /*
+             * If a new base point was founded (the returned value is different from 'nil') we go to that point by adding the calculated direction.
+             * At this point we can specify an important paramterer - the work step can be bigger than the sampling step by mulitplying the added direction.
+             * And at the end we append the new point to foundedPoints array and print some logs.
+             */
             if let xB = searchPoint(currentBasePoint: argument), f(xB) < f(argument){
                 direction = (xB - argument)
                 argument = argument + direction
                 foundedPoints.append(argument)
                 statusBox.insertText("Current point: \(argument)\n")
                 statusBox.insertText("Value in the current point: \(f(argument)) \n\n")
+            /*
+             * If no new base point was founded then we go back (by going in the oposite direction stored earlier).
+             * To stop the algorithm from going more then one step back and force it instead to decrease the step or stop the program in the best point, the direction must be equal to zero!
+             * The last part is to decrease the step by the given factor.
+             */
             } else {
                 argument = argument - direction
                 statusBox.insertText("Move back to point: \(argument)\n")
@@ -105,6 +135,8 @@ struct HookJavesOptimazer {
             }
             iteration += 1
         }
+        
+        // When the end condition was reached, the function return the founded solution, which consists of the founded best point, the optimized functions value in this point and the path stored in the foundedPoints array.
         statusBox.insertText("Search is complete.\nA solution was finded in point: \(argument) with a value of f(X) = \(f(argument))\n")
         statusBox.insertText("The task took \(iteration) iterations.")
         return (argument, f(argument), foundedPoints)
